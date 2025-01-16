064d005f3127ef1f691d8c46d2844634a5d668a0
Paul K <spkim0921@gmail.com>
2025-01-16 13:18:09 -0800

    5th commit
    building home main page

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,30 +6,30 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div className="bg-[#242424] text-white p-3 pt-[100px]">
-      <div>
-        Super long content Super long content Super long content Super long
-        content Super long content
-      </div>
-      <div>
-        Super long content Super long content Super long content Super long
-        content Super long content
-      </div>
-      <div>
-        Super long content Super long content Super long content Super long
-        content Super long content
+    <main className="flex-1 bg-[#242424] text-white p-3 pt-[100px]">
+      <div className="text-center pb-96">
+        <div className="text-3xl md:text-8xl font-bold py-10">
+          <div>From idea to prototype, in</div>
+          <div>moments</div>
+        </div>
+        <div className="pb-5 md:text-2xl">
+          Validate product ideas with an all-new approach to interactive
+          prototypes and build software like never before.
+        </div>
+        <div className="flex flex-col mx-auto">
+          <div className="md:flex mx-auto tracking-widest">
+            <div className="rounded bg-gradient-to-r from-[#6754B7] to-[#B754B3] py-2 w-[250px] m-3">
+              SIGN UP
+            </div>
+            <div className="border py-2 w-[250px] m-3">SCHEDULE A DEMO</div>
+          </div>
+        </div>
       </div>
-      <div>
-        Super long content Super long content Super long content Super long
-        content Super long content
-      </div>
-      <div>
-        Super long content Super long content Super long content Super long
-        content Super long content
-      </div>
-      <div>
-        Super long content Super long content Super long content Super long
-        content Super long content
+      <div className="text-center">
+        <div className="text-2xl md:text-6xl font-bold">
+          <div>Faster experiments, more time with customers</div>
+          <div>customers</div>
+        </div>
       </div>
       <div>
         Super long content Super long content Super long content Super long
@@ -359,6 +359,6 @@ function RouteComponent() {
         Super long content Super long content Super long content Super long
         content Super long content
       </div>
-    </div>
+    </main>
   );
 }
