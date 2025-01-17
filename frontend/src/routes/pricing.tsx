35fa4501e3e3c993f7ab99f86dc868fd9ecf7d6e
Paul K <spkim0921@gmail.com>
2025-01-16 17:09:07 -0800

    17th commit

--- frontend/src/routes/pricing.tsx
+++ frontend/src/routes/pricing.tsx
@@ -7,7 +7,16 @@ export const Route = createFileRoute("/pricing")({
 function RouteComponent() {
   return (
     <main className="flex-1 bg-[#242424] text-white p-3 pt-[100px]">
-      Hello "/pricing"!
+      <div className="md:p-10">
+        <div className="nunitofont tracking-widest text-center text-xl pb-5 md:pb-0">
+          PRICING
+        </div>
+      </div>
+      <div className="text-center pb-20">
+        <div className="text-2xl md:text-6xl font-bold">
+          <div>Choose the plan that works best for you</div>
+        </div>
+      </div>
     </main>
   );
 }
