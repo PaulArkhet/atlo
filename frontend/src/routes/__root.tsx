31f4efd39f7334a62f3973879d94b9f26a78f2bd
Paul K <spkim0921@gmail.com>
2025-01-16 16:02:35 -0800

    13th commit implemented scrollToTop

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { ScrollToTop } from "../ScrollToTop";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -11,6 +12,7 @@ function RootComponent() {
   return (
     <React.Fragment>
       <div className="flex flex-col min-h-screen">
+        <ScrollToTop />
         <Header />
         <Outlet />
         <Footer />
