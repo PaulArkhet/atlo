da865b1e452eb1ae0363f1c3fe374ca371750634
Paul K <spkim0921@gmail.com>
2025-01-20 19:11:37 -0800

    21st commit building about page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { Link, RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => {
+    return (
+      <main className="flex-1 bg-[#242424] text-white p-3 pt-[100px]">
+        <p className="text-white text-xl md:text-4xl text-center pt-10 md:pt-32">
+          Whoops! This isn't what you're looking for 😅
+        </p>
+        <div className="flex flex-col my-10 md:my-20 mx-auto w-[250px]">
+          <Link
+            to="/"
+            className="nunitofont py-2 px-10 rounded bg-[#9253E4] text-center tracking-widest"
+          >
+            LET'S GO HOME
+          </Link>
+        </div>
+      </main>
+    );
+  },
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
--- frontend/src/routes/about.tsx
+++ frontend/src/routes/about.tsx
@@ -1,5 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import maskGroup from "/aboutmaskgroup.svg";
+import nate from "/aboutnate.png";
+import randall from "/aboutrandall.png";
+import vitor from "/aboutvitor.png";
+import paul from "/aboutpaul.png";
+import selina from "/aboutselina.png";
+import steph from "/aboutsteph.png";
 
 export const Route = createFileRoute("/about")({
   component: RouteComponent,
@@ -7,17 +13,17 @@ export const Route = createFileRoute("/about")({
 
 function RouteComponent() {
   return (
-    <main className="flex-1 bg-[#242424] text-white p-3 pt-[100px]">
+    <main className="flex-1 bg-[#242424] p-3 pt-[100px] text-[#D9D9D9] md:text-2xl">
       <div className="md:p-10">
         <div className="nunitofont tracking-widest text-center text-xl pb-5 md:pb-0">
           ABOUT
         </div>
       </div>
       <div className="text-center pb-20">
-        <div className="text-2xl md:text-6xl font-bold">
+        <div className="text-2xl md:text-6xl font-bold text-white ">
           <div>The humanity of design cannot be automated</div>
         </div>
-        <div className="py-7 md:text-2xl pb-10">
+        <div className="py-7 pb-10">
           <div className="">
             We're on a mission to transform the future of the design industry by
             making it possible for anyone to <br /> build fully interactive
@@ -25,6 +31,57 @@ function RouteComponent() {
           </div>
         </div>
         <img src={maskGroup} alt="" className="mx-auto" />
+        <div className="">
+          Arkhet applies AI at specific points in the product process to and
+          accelerate meaningful experiences. <br /> And unlike "AI Design Tools"
+          that require prompt (re)writing, Arkhet products let you build out
+          your <br /> idea visually.
+        </div>
+      </div>
+      <div className="flex flex-col mx-auto">
+        <div className="flex mx-auto">
+          <div className="text-4xl font-bold text-white mr-10">
+            Product leaders can now <br /> design and build <br /> software
+            products like <br /> never before
+          </div>
+          <div className="w-[550px] ml-10">
+            <div className="mb-10">
+              Arkhet products empower product teams to build great products.
+              Have an idea for great software? Now you can bring it to life in
+              moments with interactive prototypes.{" "}
+            </div>
+            <div className="my-10">
+              Invite users to interact with your idea, in real time, in a single
+              sitting.{" "}
+            </div>
+            <div className="my-10">
+              The effort (and pain) to build great software, de-risk and test
+              it, apply feedback, and do it all again, (and again), required
+              resources unavailable to many.
+            </div>
+            <div>Until now.</div>
+          </div>
+        </div>
+      </div>
+      <div className="py-20 text-3xl font-bold text-center">
+        Building great software is easier with better tools. <br /> Arkhet
+        builds tools for builders.
+      </div>
+      <div className="h-[1px] bg-white w-[500px] mx-auto mb-20"></div>
+      <div className="nunitofont text-center py-10 tracking-widest">TEAM</div>
+      <div className="text-2xl md:text-6xl font-bold text-white text-center mb-20">
+        We’re shaping the future of generative <br />
+        software design with a mission-driven team
+      </div>
+      <div className="flex flex-col mx-auto pb-32">
+        <div className="grid grid-cols-3 gap-6 mx-auto">
+          <img src={nate} alt="" />
+          <img src={randall} alt="" />
+          <img src={vitor} alt="" />
+          <img src={paul} alt="" />
+          <img src={selina} alt="" />
+          <img src={steph} alt="" />
+        </div>
       </div>
     </main>
   );
